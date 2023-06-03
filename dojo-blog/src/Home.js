15fe16b1ece7c975d7c46c236ddb2c549eeaf5af
Pat P <patrickeperkins@gmail.com>
2023-06-03 15:10:58 -0400

    lesson 7 commit

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('Hello Ninjas', e)
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log(`hello ${name}`, e.screenX)
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => handleClickAgain('Patrick', e)}>Click Me Again</button>
         </div>
      );
 }
