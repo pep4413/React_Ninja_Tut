543902663c871138cf5fbb9e6e50e0a16c9d76d5
Pat P <patrickeperkins@gmail.com>
2023-06-03 19:53:06 -0400

    lesson 10 commit

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,30 +1,20 @@
 import { useState } from 'react'
 
 const Home = () => {
-    // let name = 'Patrick'
-    const [name, setName] = useState('Patrick')
-    const [age, setAge] = useState(44)
-    const [clickCount, setClickCount] = useState(true)
-
-    const handleClick = () => {
-        if (clickCount) {
-            setName("Luigi")
-            setAge(22)
-            setClickCount(false)
-        } else {
-            setName("Patrick")
-            setAge(44)
-            setClickCount(true)
-        }
-        console.log(clickCount)
-    }
-
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
