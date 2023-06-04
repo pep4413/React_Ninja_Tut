75ecaa7f22f61a39afeb9db162d90107a3ff3e3d
Pat P <patrickeperkins@gmail.com>
2023-06-04 01:38:49 -0400

    lesson 15 commit

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [name, setName] = useState('mario')
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs)
@@ -15,13 +17,14 @@ const Home = () => {
 
     useEffect(() => {
         console.log('use effect')
-        console.log(blogs)
-    })
+        console.log(name)
+    }, [name])
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-
+            <button onClick={() => setName('luigi')}>changeName</button>
+            <p>{name}</p>
         </div>
      );
 }
