3c9dbf57edc9fbb611bf54500238520a6e2f0d02
Pat P <patrickeperkins@gmail.com>
2023-06-05 09:19:31 -0400

    lesson 24 commit

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -7,8 +7,10 @@ const useFetch = (url) => {
 
 
     useEffect(() => {
+        const abortCont = new AbortController()
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if(!res.ok) {
                     throw Error('Could not fetch the data for that resource')
@@ -21,10 +23,16 @@ const useFetch = (url) => {
                 setError(null)
             })
             .catch((err) => {
-                setError(err.message)
-                setIsPending(false)
+                if(err.name === 'AbortError') {
+                    console.log('fetch aborted')
+                } else {
+                    setError(err.message)
+                    setIsPending(false)
+                }
             })
         }, 1000)
+
+        return () => abortCont.abort()
     }, [url])
 
 
